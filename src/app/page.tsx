d83734ba113a277a263aef61f330e6a4554e6e1e
ChunWarayut <warayut2555@gmail.com>
2023-10-23 23:37:20 +0700

    save

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -204,7 +204,7 @@ export default function Home () {
                             {origUrl}
                           </td>
                           <td className='px-6 py-3 border-b border-gray-200 text-left text-xs leading-4 font-medium text-gray-900 uppercase tracking-wider'>
-                            {shortUrl}
+                            <a href={shortUrl} target='_blank'className='text-blue-600'>{shortUrl}</a>
                           </td>
                         </tr>
                       )
